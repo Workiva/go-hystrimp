ba8e963ca478fc418a2174782e1320d0d0a2695a
Tom Deering <tom.deering@workiva.com>
2015-10-08 13:21:24 -0500

    Initial release of go-hystrimp.

---
+++ hystrimp/semaphore.go
@@ -0,0 +1,21 @@
+package hystrimp
+
+import log "github.com/Sirupsen/logrus"
+
+type semaphore chan struct{}
+
+func newSemaphore(size int) semaphore {
+	return semaphore(make(chan struct{}, size))
+}
+
+func (s semaphore) up() {
+	select {
+	case <-s:
+	default:
+		log.Warningln("Attempt to up semaphore beyond its capacity was unexpected and probably indicates a bug!")
+	}
+}
+
+func (s semaphore) down() {
+	s <- struct{}{}
+}
